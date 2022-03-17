db8ef7e7bbaf53f18f997e67aef2d4a4d6ae9050
izaowl <izasow@yahoo.com>
2022-03-17 17:09:25 +0000

    Resolution with test

---
+++ allLongestStrings/allLongestStrings.js
@@ -0,0 +1,15 @@
+function allLongestStrings(str) {
+    const extractedSubstrings =  str.toString().split(',');
+    const lengthOfSubstrings = extractedSubstrings.map(function (subString){
+        return subString.length;
+    });
+     const longestSubstring = Math.max(...lengthOfSubstrings);
+   result = []
+   for (var iteratingSubstrings = 0; iteratingSubstrings < extractedSubstrings.length; iteratingSubstrings++){
+       if (extractedSubstrings[iteratingSubstrings].length === longestSubstring)
+       result.push(extractedSubstrings[iteratingSubstrings])
+   }
+return result;
+}
+
+module.exports = allLongestStrings;
---
+++ allLongestStrings/allLongestStrings.test.js
@@ -0,0 +1,16 @@
+
+
+const allLongestStrings = require ('./allLongestStrings');
+
+describe(allLongestStrings.name, () => {
+    it('Test 1', () => {
+        // arrange
+        const data = ["aba", "aa", "ad", "vcd", "aba"];
+
+        // act
+        const response = allLongestStrings(data);
+
+        // // assert
+        expect(response).toEqual(["aba", "vcd", "aba"]);
+    });
+});
