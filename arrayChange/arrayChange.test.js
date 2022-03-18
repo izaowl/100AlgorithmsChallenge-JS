75e355c1b4bdecc24ce184687de06ff75e6ab80f
izaowl <izasow@yahoo.com>
2022-03-18 11:52:45 +0000

    Solution and test

---
+++ arrayChange/arrayChange.test.js
@@ -0,0 +1,14 @@
+const arrayChange = require('./arrayChange');
+
+describe(arrayChange.name, () => {
+    it('Test 1', () => {
+        // arrange
+        const data = [1, 1, 1];
+
+        // act
+        const response = arrayChange(data);
+
+        // assert
+        expect(response).toBe(3);
+    });
+});
