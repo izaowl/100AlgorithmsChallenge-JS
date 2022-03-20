aec533930c7fd6065679ce1c9eefc3605ef93419
izaowl <izasow@yahoo.com>
2022-03-20 22:11:18 +0000

    Initial set-up

---
+++ alphabetSubsequence/alphabetSubsequence.js
@@ -0,0 +1,10 @@
+function alphabetSubsequence(string){
+
+}
+module.exports = alphabetSubsequence;
+
+console.log(alphabetSubsequence('zab'))
+console.log(alphabetSubsequence('effg'))
+console.log(alphabetSubsequence('cdce'))
+console.log(alphabetSubsequence('ace'))
+console.log(alphabetSubsequence('bxz'))
